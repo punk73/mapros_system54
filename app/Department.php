345daba52c67a3d955bade5ed9bc23faa132f226
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-04 14:15:02 +0700

    implement log in department model

--- app/Department.php
+++ app/Department.php
@@ -3,10 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogsActivityInterface;
+use Spatie\Activitylog\LogsActivity;
 
-class Department extends Model
+class Department extends Model implements LogsActivityInterface
 {
+    use LogsActivity;
+
     protected $table = 'departments';
+
+    public function getActivityDescriptionForEvent($eventName)
+	{
+	    if ($eventName == 'created')
+	    {
+	        return 'Department "' . $this->name . '" was created';
+	    }
+
+	    if ($eventName == 'updated')
+	    {
+	        return 'Department "' . $this->name . '" was updated';
+	    }
+
+	    if ($eventName == 'deleted')
+	    {
+	        return 'Department "' . $this->name . '" was deleted';
+	    }
+
+	    return '';
+	}
 
     public function users(){
     	return $this->hasMany('App\User');
