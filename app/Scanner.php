6314f6a841a66436120328f541698408f025b0d5
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-04 14:21:31 +0700

    implement log in scanner

--- app/Scanner.php
+++ app/Scanner.php
@@ -3,8 +3,34 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogsActivityInterface;
+use Spatie\Activitylog\LogsActivity;
 
-class Scanner extends Model
-{
+
+class Scanner extends Model implements LogsActivityInterface
+{	
+	use LogsActivity; //use Logs traits
+
     protected $table = 'scanners';
+
+    public function getActivityDescriptionForEvent($eventName)
+	{
+	    if ($eventName == 'created')
+	    {
+	        return $this->table .' "'. $this->name . '" was created';
+	    }
+
+	    if ($eventName == 'updated')
+	    {
+	        return $this->table .' "'. $this->name . '" was updated';
+	    }
+
+	    if ($eventName == 'deleted')
+	    {
+	        return $this->table .' "'. $this->name . '" was deleted';
+	    }
+
+	    return '';
+	}
+
 }
