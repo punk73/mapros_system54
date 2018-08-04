7e286b5675d5244bd7342b4f05f2f1766a9557e3
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-04 12:16:15 +0700

    revise so users seeder in last

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,19 +13,19 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(ScannerSeeder::class);
-        $this->call(UsersTableSeeder::class);
         $this->call(SequenceSeeder::class);
         $this->call(LineprocessSeeder::class);
         $this->call(EndpointSeeder::class);
         $this->call(LineSeeder::class);
         $this->call(LinetypeSeeder::class);
         $this->call(VoyagerDatabaseSeeder::class);
-        
+        // user seeder should be in the last
+        $this->call(UsersTableSeeder::class);
         // $this->call(DataRowsTableSeeder::class);
         // $this->call(DataTypesTableSeeder::class);
         // $this->call(MenuItemsTableSeeder::class);
         // $this->call(SettingsTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
+        // $this->call(PermissionsTableSeeder::class);
+        // $this->call(PermissionRoleTableSeeder::class);
     }
 }
