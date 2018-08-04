742d5b2946966605e5c2c340939cdf0b35050609
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-04 07:04:14 +0700

    add nik into register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,6 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'nik' => $data['nik'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
