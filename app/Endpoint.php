083177accf66cb5ff6af3e83bd12d47cadf87b9d
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-04 14:46:22 +0700

    implement log into endpoint & line table

--- app/Endpoint.php
+++ app/Endpoint.php
@@ -3,8 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogsActivityInterface;
+use Spatie\Activitylog\LogsActivity;
 
-class Endpoint extends Model
+class Endpoint extends Model implements LogsActivityInterface
 {
-    //
+    protected $table = 'endpoints';
+
+    use LogsActivity; //use Logs traits
+
+    public function getActivityDescriptionForEvent($eventName)
+	{
+	    if ($eventName == 'created')
+	    {
+	        return $this->table .' "'. $this->name . '" was created';
+	    }
+
+	    if ($eventName == 'updated')
+	    {
+	        return $this->table .' "'. $this->name . '" was updated';
+	    }
+
+	    if ($eventName == 'deleted')
+	    {
+	        return $this->table .' "'. $this->name . '" was deleted';
+	    }
+
+	    return '';
+	}
 }
