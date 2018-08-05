b3c018c3f1cae32c5b1734712f9d38ff37fda691
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-05 10:57:11 +0700

    add create admin command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\SaveVoyager;
+use App\Console\Commands\AdminCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +17,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         // register command class
         SaveVoyager::class,
+        AdminCommand::class,
     ];
 
     /**
