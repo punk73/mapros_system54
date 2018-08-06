bda60a978bb2dee49c73047337b32e8859bd5883
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-08-06 11:46:42 +0700

    revise seeder sequences

--- database/seeds/SequenceSeeder.php
+++ database/seeds/SequenceSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Sequence;
+use App\Mastermodel;
 
 class SequenceSeeder extends Seeder
 {
@@ -16,9 +17,11 @@ class SequenceSeeder extends Seeder
         foreach ($sequences as $key => $data) {
             # code...
             $sequence = new Sequence;
-            $sequence->name = $data[0];
-            $sequence->line_id = $data[1];
-            $sequence->process = $data[2];
+            $sequence->name = $data['name'];
+            $sequence->modelname = $data['modelname'];
+            $sequence->pwbname = $data['pwbname'];
+            $sequence->line_id = $data['line_id'];
+            $sequence->process = $data['process'];
             $sequence->save();
         }
 
@@ -26,12 +29,29 @@ class SequenceSeeder extends Seeder
 
     public function getData(){
         // name    line_id process
-        return [
+        $Mastermodel = Mastermodel::take(20)->get();;
+
+        $process = [
             ['PNL',     1,   '13,14,15,16,17,20'],
             ['MST',     1,   '18,19,20,27,35,28,29,30,31,32,33,36,37,38,39,40'],
             ['MAIN',    1,   '1,3,4,7,9,10,12,18'],
             ['Daughter',1,   '1,5,7,18'],
             ['Switch',  1,   '1,5,7,8,13'],
         ];
+
+
+        $data = [];
+        foreach ($Mastermodel as $key => $model) {
+            $item = [];
+            $item['name']       = $model['name'];;
+            $item['modelname']  = $model['name'];
+            $item['pwbname']    = $model['pwbname'];
+            $item['process']    = $process[rand(0,4)][2];
+            $item['line_id']    = 1;
+            $data[] = $item;
+        }
+
+        return $data;
+
     }
 }
