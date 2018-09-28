7be07b3e2dc08ae9d4cabb12ed7b06221f104022
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-09-28 17:03:22 +0700

    AOI connections based on env file

--- app/AOI.php
+++ app/AOI.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class AOI extends Model
 {
-	protected $connection='sqlsrv';
+	// protected $connection='sqlsrv';
+	public function __construct(){
+		$this->connection = env('DB_CONNECTION1', 'sqlsrv');
+	}
+
     protected $table = 'tblAOIResultBoard';
 
 }
