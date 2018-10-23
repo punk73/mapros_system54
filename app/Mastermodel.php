d2f3d785b6fbee2495ccb80d0be743601b4eaf10
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-10-23 09:44:44 +0700

    changes default value to production env

--- app/Mastermodel.php
+++ app/Mastermodel.php
@@ -8,7 +8,7 @@ class Mastermodel extends Model
 {	
 	public function __construct(){
 		// parent::__construct(); 
-		$this->connection = env('DB_CONNECTION2', 'mysql2');
+		$this->connection = env('DB_CONNECTION2', 'sqlsrv1'); //the default value is for productions env to avoid error
 	}
 
     protected $table = 'models';
