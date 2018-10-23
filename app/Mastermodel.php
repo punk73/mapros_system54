4493b2559c1cab8da8c262b82bb7fea2c5ba1ddc
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-10-23 12:10:13 +0700

    use config instead of env function in Mastermodel

--- app/Mastermodel.php
+++ app/Mastermodel.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App;
-
+use Config;
 use Illuminate\Database\Eloquent\Model;
 
 class Mastermodel extends Model
 {	
 	public function __construct(){
 		// parent::__construct(); 
-		$this->connection = env('DB_CONNECTION2', 'sqlsrv1'); //the default value is for productions env to avoid error
+		$this->connection = Config::get('database.connections.sqlsrv1') //env('DB_CONNECTION2', 'sqlsrv1'); //the default value is for productions env to avoid error
 	}
 
     protected $table = 'models';
