cefda562258cc748b8bef8fe7c58d3cc6cef8727
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-11-22 14:59:35 +0700

    add model & migrations for locations

---
+++ database/migrations/2018_11_22_142625_create_table_locations.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableLocations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('pwb_id');//->unsigned();
+            $table->string('ref_no');
+            $table->timestamps();
+
+            
+            $table->foreign('pwb_id')->references('id')->on('pwbs')->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
