8cb69e71391246723492421209e6274cc2870d02
it8 <it8@JVC-JEIN.CO.ID>
2018-12-26 15:23:51 +0700

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <link rel="shortcut icon" href="{{{ asset('favicon.ico') }}}">
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
