49921b6b5ce366a9d08d201e84bb48187b3aa20b
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-01-14 14:56:44 +0700

    activiy log on model part (lcd module table)

--- app/Part.php
+++ app/Part.php
@@ -2,9 +2,43 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Model;
+use App\BaseModel as Model;
 
 class Part extends Model
 {
     protected $table = 'parts';
+
+    public function getActivityDescriptionForEvent($eventName)
+	{
+	    if ($eventName == 'created')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was created';
+	    }
+
+	    if ($eventName == 'updated')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was updated';
+	    }
+
+	    if ($eventName == 'deleted')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was deleted';
+	    }
+
+	    return '';
+	}
+
+	private function getData(){
+		return json_encode([
+			'barcode' => $this->barcode,
+			'guid_master' => $this->guid_master,
+			'guid_ticket'	=> $this->guid_ticket,
+			'scanner_id' => $this->scanner_id,
+			'status' => $this->status,
+			'scan_nik' => $this->scan_nik,
+			'scanner_id' => $this->scanner_id,
+			'judge' => $this->judge,
+		]);
+	}
 }
