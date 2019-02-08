00c76e8130b44970968b784875b04b21bd9e943f
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-02-08 09:55:12 +0700

    perbaiki controller Aoi

--- app/Api/V1/Controllers/AoiController.php
+++ app/Api/V1/Controllers/AoiController.php
@@ -4,6 +4,7 @@ namespace App\Api\V1\Controllers;
 
 use App\Http\Controllers\Controller;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Dingo\Api\Exception\StoreResourceFailedException;
 use Illuminate\Http\Request;
 use App\Api\V1\Requests\AoiRequest;
 use App\AOI;
@@ -15,6 +16,12 @@ class AoiController extends Controller
 			'barcode',
 			'userjudgment'
 		])->where('barcode', $request->board_id );
+		
+		if(!$aoi->first() ){
+			throw new StoreResourceFailedException("data tidak ditemukan '{$request->board_id}' di SMT!!", [
+				'message' => 'data tidak ditemukan pada table AOI!'
+			]);
+		}
 
 		return [
 			'success' => true,
