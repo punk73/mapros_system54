6dcea9a9a27e0355de87b60aaaaa3d619e6756c5
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-03-23 08:29:28 +0700

    add id on copy button

--- resources/views/vendor/voyager/sequences/browse.blade.php
+++ resources/views/vendor/voyager/sequences/browse.blade.php
@@ -191,8 +191,7 @@
                                             @foreach(Voyager::actions() as $action)
                                                 @include('voyager::bread.partials.actions', ['action' => $action])
                                             @endforeach
-
-                                            <a href="#" title="Copy" class="btn btn-sm btn-success pull-right view" style="margin-right:3px" >
+                                            <a href="#" id="edit-{{ $data->id }}" title="Copy" class="btn btn-sm btn-success pull-right view" style="margin-right:3px" >
                                                 <i class="voyager-edit"></i> <span class="hidden-xs hidden-sm">Copy</span>
                                             </a>
                                         </td>
