707f3ae08ce91dfaff420d98ed47816304efa24b
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-03-25 13:18:03 +0700

    fixing paginate method on firebird

--- app/Weightcontrol.php
+++ app/Weightcontrol.php
@@ -4,12 +4,26 @@ namespace App;
 
 // use Illuminate\Database\Eloquent\Model;
 use Firebird\Model;
+use App\Database\Query\FirebirdQueryBuilder as QueryBuilder;
 
 class Weightcontrol extends Model
 {
     protected $table = 'WEIGHTCONTROL';
 
     public function __construct(){
-		$this->connection = env('DB_CONNECTION_FB', 'firebird');
+      $this->connection = env('DB_CONNECTION_FB', 'firebird');
+    }
+
+    /**
+	 * Get a new query builder instance for the connection.
+	 *
+	 * @return \Illuminate\Database\Query\Builder
+	 */
+	protected function newBaseQueryBuilder() {
+		$connection = $this->getConnection();
+
+		return new QueryBuilder(
+			$connection, $connection->getQueryGrammar(), $connection->getPostProcessor()
+		);
 	}
 }
