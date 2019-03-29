cb0ead4474a8838d580b604dab546e9a7bb01c4c
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-03-29 13:32:24 +0700

    revise modelname

---
+++ app/LineprocessManualInstruction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LineprocessManualInstruction extends Model
+{
+    // protected $table = 'lineprocess_manual_intructions';
+}
