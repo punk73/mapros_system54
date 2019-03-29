dd4af3f49e4edd5cee6c9bdfc7d0749c66fb5dbf
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-03-29 11:33:30 +0700

    add lineprocess_intruction_manual migration for config

---
+++ database/migrations/2019_03_29_112336_create_table_lineprocess_instruction_manual.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableLineprocessInstructionManual extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lineprocess_instruction_manual', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('lineprocess_id');
+            $table->unsignedInteger('scanner_id');
+
+            $table->integer('has_log'); //it should be 0 or 1; boolean
+            $table->foreign('lineprocess_id')
+                ->references('id')
+                ->on('lineprocesses')
+                ->onDelete('cascade');
+            
+            $table->foreign('scanner_id')
+                ->references('id')
+                ->on('scanners')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lineprocess_instruction_manual', function (Blueprint $table) {
+            if( Schema::hasColumn('lineprocess_instruction_manual', 'lineprocess_id') ){
+                // drop foreign key
+                $table->dropForeign(['lineprocess_id']);
+            }
+
+            if( Schema::hasColumn('lineprocess_instruction_manual', 'scanner_id') ){
+                // drop foreign key
+                $table->dropForeign(['scanner_id']);
+            }
+        });
+
+        Schema::dropIfExists('lineprocess_instruction_manual');
+    }
+}
