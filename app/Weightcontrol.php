d10178514dc8c3cce0e2d010b5df22bdd466ad6e
JVCFDDOM001\it8 <teguhsetiawan171@gmail.com>
2019-05-28 16:03:24 +0700

    weighcontrol logable

--- app/Weightcontrol.php
+++ app/Weightcontrol.php
@@ -5,16 +5,21 @@ namespace App;
 // use Illuminate\Database\Eloquent\Model;
 use Firebird\Model;
 use App\Database\Query\FirebirdQueryBuilder as QueryBuilder;
+use Spatie\Activitylog\LogsActivityInterface;
+use Spatie\Activitylog\LogsActivity;
+use function GuzzleHttp\json_encode;
 
-class Weightcontrol extends Model
+class Weightcontrol extends Model implements LogsActivityInterface
 {
-		protected $table = 'WEIGHTCONTROL';
-		
-		public $timestamps = false;
+	use LogsActivity;
 
-		public $incrementing = false;
+	protected $table = 'WEIGHTCONTROL';
+	
+	public $timestamps = false;
 
-		protected $primaryKey = 'ID'; // or null
+	public $incrementing = false;
+
+	protected $primaryKey = 'ID'; // or null
 
     public function __construct(){
       $this->connection = env('DB_CONNECTION_FB', 'firebird');
@@ -32,4 +37,35 @@ class Weightcontrol extends Model
 			$connection, $connection->getQueryGrammar(), $connection->getPostProcessor()
 		);
 	}
+
+	public function getActivityDescriptionForEvent($eventName)
+	{
+	    if ($eventName == 'created')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was created';
+	    }
+
+	    if ($eventName == 'updated')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was updated';
+	    }
+
+	    if ($eventName == 'deleted')
+	    {
+	        return $this->table .' "'. $this->getData() . '" was deleted';
+	    }
+
+	    return '';
+	}
+
+	public function getData() {
+		return json_encode([
+			'MODEL_NAME' => $this->MODEL_NAME,
+			'STDWEIGHT' => $this->STDWEIGHT,
+			'TOLERANCE' => $this->TOLERANCE,
+			'SCALE' => $this->SCALE,			
+		]);
+	}
+
+
 }
