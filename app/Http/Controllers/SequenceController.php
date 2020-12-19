267e9427ad8b8f410967a25a16fb91cab6243fec
punk73 <teguhsetiawan171@gmail.com>
2020-12-19 07:54:54 +0700

    check duplication on sequence store

--- app/Http/Controllers/SequenceController.php
+++ app/Http/Controllers/SequenceController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController as Controller;
 use App\Sequence;
+use Dingo\Api\Exception\StoreResourceFailedException;
+use Exception;
 
 class SequenceController extends Controller
 {
@@ -20,4 +22,41 @@ class SequenceController extends Controller
 
         return view('vendor.voyager.sequences.copy', ['sequence' => $sequence->toArray() ]);
     }
+
+    public function store(Request $request){
+        // 
+        // return $request->all();
+        $rule = [
+            'modelname' => "required", 
+            'pwbname' => "required",
+            "line_id" => "required",
+        ];
+        $params = $this->validate($request, $rule);
+
+        $alreadyExist = Sequence::where(function ($q) use ($request, $rule) {
+            foreach($rule as $key => $value) {
+                $q->where($key, $request->{$key});
+            }
+        })->first();
+
+        if($alreadyExist) {
+            $msg = "DATA DENGAN MODEL '{$request->modelname}', PWBNAME {$request->pwbname} DAN LINE {$request->line_id} SUDAH ADA. SILAHKAN EDIT SAJA.";
+
+            if ($request->ajax()) {
+                // return response()->json(['success' => true, 'message' => $msg]);
+            }
+
+            return redirect()
+                ->route("voyager.sequences.index")
+                ->with([
+                    'message'    => $msg,
+                    'alert-type' => 'error',
+                ]);
+
+        }
+
+        return parent::store($request);
+    }
+
+    
 }
