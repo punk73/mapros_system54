8845c8d00ceb8244e34454a331beb9804f16c7d7
Harris <harris.git21@gmail.com>
2023-05-19 11:35:46 +0700

    Revise AOI check with additional convert to mothercode if cavity data AOI not found

--- app/Api/V1/Controllers/AoiController.php
+++ app/Api/V1/Controllers/AoiController.php
@@ -10,27 +10,60 @@ use App\Api\V1\Requests\AoiRequest;
 use App\AOI;
 
 class AoiController extends Controller
-{	
-	public function index(AoiRequest $request){
+{
+	public function index(AoiRequest $request)
+	{
 		$aoi = AOI::select([
 			'barcode',
 			'userjudgment'
-		])->where('barcode', $request->board_id );
-		
-		if(!$aoi->first() ){
-			// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
-			throw new StoreResourceFailedException( "Board '{$request->board_id}' belum inspect AOI atau NG AOI. Silahkan confirm SMT ", [
-				'message' => 'data tidak ditemukan pada table AOI!'
-			]);
+		])->where('barcode', $request->board_id);
+
+		if ($aoi->first()) {
+			return [
+				'success' => true,
+				'data' => $aoi->first(),
+				'status' => 'OUT',
+				'judge' => ($aoi->exists()) ? 'OK' : 'NG'
+			];
+		} else {
+			$changeToMother = $this->changeToMotherCode($request->board_id);
+			$aoi_convert = AOI::select([
+				'barcode',
+				'userjudgment'
+			])->where('barcode', $changeToMother);
+
+			if (!$aoi_convert->first()) {
+				// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
+				throw new StoreResourceFailedException("Board '{$request->board_id}' belum inspect AOI atau NG AOI. Silahkan confirm SMT ", [
+					'message' => 'data tidak ditemukan pada table AOI!'
+				]);
+			}
+
+			return [
+				'success' => true,
+				'data' => $aoi_convert->first(),
+				'status' => 'OUT',
+				'judge' => ($aoi_convert->exists()) ? 'OK' : 'NG'
+			];
 		}
 
-		return [
-			'success' => true,
-			'data' => $aoi->first(),
-			'status' => 'OUT',
-			'judge' => ($aoi->exists()) ? 'OK':'NG'
-		];
+		// if (!$aoi->first()) {
+		// 	// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
+		// 	throw new StoreResourceFailedException("Board '{$request->board_id}' belum inspect AOI atau NG AOI. Silahkan confirm SMT ", [
+		// 		'message' => 'data tidak ditemukan pada table AOI!'
+		// 	]);
+		// }
+
+		// return [
+		// 	'success' => true,
+		// 	'data' => $aoi->first(),
+		// 	'status' => 'OUT',
+		// 	'judge' => ($aoi->exists()) ? 'OK' : 'NG'
+		// ];
 	}
 
-	
+	public function changeToMotherCode($boardid)
+	{
+		return substr_replace($boardid, '00', 12, -10);
+	}
 }
