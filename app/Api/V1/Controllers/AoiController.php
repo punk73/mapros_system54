85020c81b83b66e94a40b2863ee623a2c9f2d0d3
Harris <harris.git21@gmail.com>
2023-08-21 08:21:27 +0700

    feat: add feature change from key board to do board when aoi check status for Model DT

--- app/Api/V1/Controllers/AoiController.php
+++ app/Api/V1/Controllers/AoiController.php
@@ -13,10 +13,13 @@ class AoiController extends Controller
 {
 	public function index(AoiRequest $request)
 	{
+
+		$boardid = $request->board_id;
+
 		$aoi = AOI::select([
 			'barcode',
 			'userjudgment'
-		])->where('barcode', $request->board_id);
+		])->where('barcode', $boardid);
 
 		if ($aoi->first()) {
 			return [
@@ -25,27 +28,34 @@ class AoiController extends Controller
 				'status' => 'OUT',
 				'judge' => ($aoi->exists()) ? 'OK' : 'NG'
 			];
-		} else {
-			$changeToMother = $this->changeToMotherCode($request->board_id);
-			$aoi_convert = AOI::select([
-				'barcode',
-				'userjudgment'
-			])->where('barcode', $changeToMother);
+		} 
 
-			if (!$aoi_convert->first()) {
-				// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
-				throw new StoreResourceFailedException("Board '{$request->board_id}' belum inspect AOI atau NG AOI. Silahkan confirm SMT ", [
-					'message' => 'data tidak ditemukan pada table AOI!'
-				]);
-			}
-
-			return [
-				'success' => true,
-				'data' => $aoi_convert->first(),
-				'status' => 'OUT',
-				'judge' => ($aoi_convert->exists()) ? 'OK' : 'NG'
-			];
+		// check board DT key
+		$isKeyBoard = $this->checkKeyBoard($boardid);
+		if($isKeyBoard=='true')
+		{
+			$boardid = $this->changeToDOBoard($boardid);
 		}
+
+		$changeToMother = $this->changeToMotherCode($boardid);
+		$aoi_convert = AOI::select([
+			'barcode',
+			'userjudgment'
+		])->where('barcode', $changeToMother);
+
+		if (!$aoi_convert->first()) {
+			// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
+			throw new StoreResourceFailedException("Board '{$request->board_id}' belum inspect AOI atau NG AOI. Silahkan confirm SMT ", [
+				'message' => 'data tidak ditemukan pada table AOI!'
+			]);
+		}
+
+		return [
+			'success' => true,
+			'data' => $aoi_convert->first(),
+			'status' => 'OUT',
+			'judge' => ($aoi_convert->exists()) ? 'OK' : 'NG'
+		];
 
 		// if (!$aoi->first()) {
 		// 	// "Data '{$request->board_id}' NG atau tidak ditemukan di SMT!!"
@@ -65,5 +75,27 @@ class AoiController extends Controller
 	public function changeToMotherCode($boardid)
 	{
 		return substr_replace($boardid, '00', 12, -10);
+	}
+	public function changeToDOBoard($boardid)
+	{
+		// Y-J-5-2-2-4-M-0-1-D-O-_-0-1-A-7-0-1-5-A-0-0-1-3
+		// 0-1-2-3-4-5-6-7-8-9-0-1-2-3-4-5-6-7-8-9-0-1-2-3
+		// $boardid='YJ5224M01DO_01A7015A0013';
+		return substr_replace($boardid, 'DO', 9, 2);
+	}
+
+	public function checkKeyBoard($boardid)
+	{
+		// Y-J-5-2-2-4-M-0-1-D-O-_-0-1-A-7-0-1-5-A-0-0-1-3
+		// 0-1-2-3-4-5-6-7-8-9-0-1-2-3-4-5-6-7-8-9-0-1-2-3
+		// $boardid='YJ5224M01DO_01A7015A0013';
+		
+		$is_key = substr($boardid,9,2);
+		if($is_key=="KY")
+		{
+			return 'true';
+		}
+		return 'false';
+
 	}
 }
